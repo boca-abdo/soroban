372665b46a584561220414ff0bb3be07e2d3156f
boca-abdo <boca.abdo@gmail.com>
2018-06-24 12:48:27 +0100

    Update lessons menu

--- ar/lessons/assets/menu.php
+++ ar/lessons/assets/menu.php
@@ -5,6 +5,9 @@
     <div class="dropdown-menu bg-dark rounded-0 text-center p-0" aria-labelledby="dropdownMenu1" style="left: -120px">
       <a class="dropdown-item" href="index.php">ماهو سوروبان؟</a>
       <a class="dropdown-item" href="repr.php">تمثيل اﻷعداد</a>
+      <div class="dropdown-divider m-0"></div>
+      <a class="dropdown-item" href="practice1.php">تدريب تمثيل اﻷعداد</a>
+      <div class="dropdown-divider m-0"></div>
       <a class="dropdown-item" href="simple_add.php">الجمع البسيط</a>
       <a class="dropdown-item" href="simple_sub.php">الطرح البسيط</a>
       <a class="dropdown-item" href="mob5_add.php">المبادلة بخمسة في الجمع</a>
@@ -13,8 +16,11 @@
       <a class="dropdown-item" href="mob10_sub.php">المبادلة بعشرة في الطرح</a>
       <a class="dropdown-item" href="mob105_add.php">المبادلة بخمسة وعشرة في الجمع</a>
       <a class="dropdown-item" href="mob105_sub.php">المبادلة بخمسة وعشرة في الطرح</a>
-      <a class="dropdown-item" href="simple_mul.php">الضرب البسيط</a>
-      <a class="dropdown-item" href="comp_mul.php">الضرب المركب</a>
+      <div class="dropdown-divider m-0"></div>
+      <a class="dropdown-item" href="simple_mul1.php">الضرب البسيط - الطريقة 1 -</a>
+      <a class="dropdown-item" href="comp_mul1.php">الضرب المركب - الطريقة 1 -</a>
+      <a class="dropdown-item" href="simple_mul2.php">الضرب البسيط - الطريقة 2 -</a>
+      <a class="dropdown-item" href="comp_mul2.php">الضرب المركب - الطريقة 2 -</a>
       <a class="dropdown-item" href="division.php">القسمة</a>
     </div>
   </div>
