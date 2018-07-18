6291ae87fda3b1a3540bb5b8816abc52a3e0fc77
boca-abdo <boca.abdo@gmail.com>
2018-07-18 18:34:55 +0100

    Update lessons header

--- ar/lessons/assets/header.php
+++ ar/lessons/assets/header.php
@@ -23,7 +23,7 @@
 </head>
 <body class="bg-warning text-dark">
   <div class="navbar fixed-top justify-content-between bg-dark p-0" style="z-index: 5005">
-    <a class="btn btn-outline-warning rounded-0 border-right-0 border-left-0 border-top-0 border-bottom-0" href="index.php" style="box-shadow:none"><i class="fas fa-home"></i></a>
+    <a class="btn btn-outline-warning rounded-0 border-right-0 border-left-0 border-top-0 border-bottom-0" href="../dashboard.php" style="box-shadow:none"><i class="fas fa-home"></i></a>
     <div class="dropdown open">
       <button class="btn btn-outline-warning rounded-0 border-right-0 border-left-0 border-top-0 border-bottom-0 dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="box-shadow:none"><i class="fas fa-list-ol"></i></button>
       <div class="dropdown-menu bg-dark rounded-0 text-center p-0" aria-labelledby="dropdownMenu1" style="left: -120px;z-index: 9999">
@@ -52,5 +52,5 @@
         <a class="dropdown-item bg-danger" href="certificate.php">تحميل الشهادة</a>
       </div>
     </div>
-    <a class="btn btn-outline-warning rounded-0 border-right-0 border-left-0 border-top-0 border-bottom-0" href="../dashboard.php" style="box-shadow:none"><i class="fas fa-power-off"></i></a>
+    <a class="btn btn-outline-warning rounded-0 border-right-0 border-left-0 border-top-0 border-bottom-0" href="../../includes/logout.php?q=ar" style="box-shadow:none"><i class="fas fa-power-off"></i></a>
   </div>
