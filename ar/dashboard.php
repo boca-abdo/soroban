6d0a9a8a5385c7dd85d32c15ef982e008dde45f0
boca-abdo <boca.abdo@gmail.com>
2018-07-21 20:25:45 +0100

    Add list modal to dashboard

--- ar/dashboard.php
+++ ar/dashboard.php
@@ -1,6 +1,17 @@
 <?php include 'assets/header.php' ?>
 				<h2 class="h2">سينتهي هذا اﻷسبوع بعد</h2>
-				<h5 class="h3 text-danger animated pulse infinite font-weight-bold" id="nextweek"></h3>
+				<div class="row justify-content-center" id="nextweek">
+					<div class="col-auto h4 font-weight-bold rounded-circle bg-dark text-warning" style="height:60px; width:60px; padding-top: 13px"></div>
+					<div class="col-auto h4 font-weight-bold rounded-circle bg-dark text-warning" style="height:60px; width:60px; padding-top: 13px"></div>
+					<div class="col-auto h4 font-weight-bold rounded-circle bg-dark text-warning" style="height:60px; width:60px; padding-top: 13px"></div>
+					<div class="col-auto h4 font-weight-bold rounded-circle bg-dark text-warning" style="height:60px; width:60px; padding-top: 13px"></div>
+				</div>
+				<div class="row justify-content-center">
+					<div class="" style="width:60px"></div>
+					<div class="" style="width:60px"></div>
+					<div class="" style="width:60px"></div>
+					<div class="" style="width:60px"></div>
+				</div>
 				<div class="row justify-content-center">
 		      <div class="col-md-6 pt-3">
 		        <div class="card bg-dark text-warning rounded-0">
@@ -28,6 +39,7 @@
 			                </thead>
 			                <tbody></tbody>
 			              </table>
+										<button type="button" class="btn btn-outline-warning rounded-0 d-block mt-3 mx-auto" style="box-shadow:none" data-toggle="modal" data-target="#Mymodal" data-list="bw">الترتيب الكامل<i class="fas fa-list-ol mr-2"></i></button>
 			            </div>
 			            <div class="tab-pane fade" id="all1" role="tabpanel" aria-labelledby="all-tab">
 										<table class="table table-sm table-hover table-bordered table-striped text-right">
@@ -40,6 +52,7 @@
 			                </thead>
 			                <tbody></tbody>
 			              </table>
+										<button type="button" class="btn btn-outline-warning rounded-0 d-block mt-3 mx-auto" style="box-shadow:none" data-toggle="modal" data-target="#Mymodal" data-list="bt">الترتيب الكامل<i class="fas fa-list-ol mr-2"></i></button>
 			            </div>
 			          </div>
 		          </div>
@@ -74,6 +87,7 @@
 			                </thead>
 			                <tbody></tbody>
 			              </table>
+										<button type="button" class="btn btn-outline-warning rounded-0 d-block mt-3 mx-auto" style="box-shadow:none" data-toggle="modal" data-target="#Mymodal" data-list="ew">الترتيب الكامل<i class="fas fa-list-ol mr-2"></i></button>
 			            </div>
 			            <div class="tab-pane fade" id="all2" role="tabpanel" aria-labelledby="all2-tab">
 										<table class="table table-sm table-hover table-bordered table-striped text-right">
@@ -86,6 +100,7 @@
 			                </thead>
 			                <tbody></tbody>
 			              </table>
+										<button type="button" class="btn btn-outline-warning rounded-0 d-block mt-3 mx-auto" style="box-shadow:none" data-toggle="modal" data-target="#Mymodal" data-list="et">الترتيب الكامل<i class="fas fa-list-ol mr-2"></i></button>
 			            </div>
 			          </div>
 		          </div>
@@ -97,20 +112,49 @@
 				</div>
 	    </div>
 	  </div>
+		<div id="Mymodal" class="modal fade bd-example-modal-lg mt-5">
+			<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-content bg-dark text-warning text-center">
+					<div class="modal-header p-0">
+						<h3 class="modal-title mx-auto p-3">الترتيب الكامل</h3>
+						<button type="button" class="btn btn-sm btn-danger rounded-0" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true"><i class="fa fa-times"></i></span>
+						</button>
+					</div>
+					<div class="modal-body p-0">
+						<div class="table-responsive">
+						  <table class="table table-sm table-dark text-warning">
+						    <thead>
+									<th>المركز</th>
+									<th>العضو</th>
+									<th>المدينة</th>
+									<th>اﻷستاذ</th>
+						    	<th>النقاط</th>
+						    </thead>
+								<tbody></tbody>
+						  </table>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 		<?php include 'assets/footer.php' ?>
 	</div>
   <script type="text/javascript">
     $(document).ready(function() {
 			$("#spinner").addClass("d-none");
-			var username = "<?php echo $log_row['fname'].' '.$log_row['lname'] ?>",
+			var days,hours,minutes,seconds,days_disp,hours_disp,minutes_disp,seconds_disp,
+			username = "<?php echo $log_row['fname'].' '.$log_row['lname'] ?>",
 			points = [];
 			points["bw"] = <?php echo $log_row['b_points'] ?>;
 			points["ew"] = <?php echo $log_row['e_points'] ?>;
-			function getData(tbl,tbody) {
+			function getData(lst,tbl,tbody) {
+				tbody.html("");
 				$.ajax({
 					url: "../includes/getlist.php",
 					type: "POST",
 					data: {
+						lst: lst,
 						tbl: tbl
 					},
 					error: function(stt,xhr,err) {
@@ -119,38 +163,90 @@
 					success: function(res) {
 						var data = JSON.parse(res);
 						for (var i = 0; i < data.length; i++) {
-							tbody.append("<tr><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td><td>"+data[i][2]+"</td></tr>");
+							if (lst == "nor") {
+								tbody.append("<tr><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td><td>"+data[i][2]+"</td></tr>");
+							}
+							if (lst == "mdl") {
+								tbody.append("<tr><td>"+data[i][0]+"</td><td>"+data[i][1]+"</td><td>"+data[i][2]+"</td><td>"+data[i][3]+"</td><td>"+data[i][4]+"</td></tr>");
+							}
 						}
 					},
 					complete: function() {
 						row = tbody.find("tr");
-						if (row.length == 4) {
+						if (row.length == 4 && lst == "nor") {
 							row.last().addClass("bg-warning text-dark").before("<tr><td colspan='3' class='text-center'>مركزك الحالي هو</td></tr>");
 						}
 						if (row.length == 0) {
 							tbody.parent().after("<a class='text-warning animated bounceIn infinite' href='challenge.php'>كن أول من يسجل نقاطا هذا اﻷسبوع من هنا</a>");
 						}
-						if ((tbl == "bw" && row.length != 0) || (tbl == "ew" && row.length != 0)) {
-							var names = "";
+
+						if (lst == "mdl") {
 							row.each(function(){
-								names += $(this).find("td:eq(1)").text();
+								if ($(this).find("td:eq(1)").text() == username) {
+									$(this).addClass("bg-warning text-dark font-weight-bold");
+								}
 							});
-							if (names.search(username) == -1) {
-								tbody.parent().after("<a class='text-warning animated bounceIn infinite' href='challenge.php'>سجل أول نقاطك لهذا اﻷسبوع من هنا</a>");
-							} else {
-								if (points[tbl] != 100) {
-									tbody.parent().after("<a class='text-warning animated bounceIn infinite' href='challenge.php'>أحصل على نقاط أكثر من هنا</a>");
+						} else {
+							if ((tbl == "bw" && row.length != 0) || (tbl == "ew" && row.length != 0)) {
+								var names = "";
+								row.each(function(){
+									names += $(this).find("td:eq(1)").text();
+								});
+								if (names.search(username) == -1) {
+									tbody.parent().after("<a class='text-warning animated bounceIn infinite' href='challenge.php'>سجل أول نقاطك لهذا اﻷسبوع من هنا</a>");
+								} else {
+									if (points[tbl] != 100) {
+										tbody.parent().after("<a class='text-warning animated bounceIn infinite' href='challenge.php'>أحصل على نقاط أكثر من هنا</a>");
+									}
 								}
 							}
 						}
 					}
 				});
 			}
-			getData("bw",$("tbody:eq(0)"));
-			getData("bt",$("tbody:eq(1)"));
-			getData("ew",$("tbody:eq(2)"));
-			getData("et",$("tbody:eq(3)"));
-			$("#nextweek").countdown("<?php echo $next_monday; ?>", function(event) {$(this).text(event.strftime('%D:%H:%M:%S'));});
+			getData("nor","bw",$("tbody:eq(0)"));
+			getData("nor","bt",$("tbody:eq(1)"));
+			getData("nor","ew",$("tbody:eq(2)"));
+			getData("nor","et",$("tbody:eq(3)"));
+			$("#Mymodal").on('show.bs.modal', function (e) {
+				var button = $(e.relatedTarget);
+  			var recipient = button.data('list');
+				getData("mdl",recipient,$(this).find("tbody"));
+			});
+			$("#nextweek").countdown("<?php echo $next_monday; ?>", function(event) {
+				days = Number(event.strftime("%D"));
+				hours = Number(event.strftime("%H"));
+				minutes = Number(event.strftime("%M"));
+				seconds = Number(event.strftime("%S"));
+				if (days < 3) {
+					days_disp = "يوم";
+				} else {
+					days_disp = "أيام";
+				}
+				if (hours > 1 && hours < 11) {
+					hours_disp = "ساعات";
+				} else {
+					hours_disp = "ساعة";
+				}
+				if (minutes > 1 && minutes < 11) {
+					minutes_disp = "دقائق";
+				} else {
+					minutes_disp = "دقيقة";
+				}
+				if (seconds > 1 && seconds < 11) {
+					seconds_disp = "ثواني";
+				} else {
+					seconds_disp = "ثانية";
+				}
+				$(this).find("div:eq(0)").text(event.strftime('%D'));
+				$(this).find("div:eq(1)").text(event.strftime('%H'));
+				$(this).find("div:eq(2)").text(event.strftime('%M'));
+				$(this).find("div:eq(3)").text(event.strftime('%S'));
+				$(this).next().find("div:eq(0)").text(days_disp);
+				$(this).next().find("div:eq(1)").text(hours_disp);
+				$(this).next().find("div:eq(2)").text(minutes_disp);
+				$(this).next().find("div:eq(3)").text(seconds_disp);
+			});
     });
   </script>
 </body>
--- includes/getlist.php
+++ includes/getlist.php
@@ -3,18 +3,19 @@
   require_once 'user_check.php';
   $data = array();
   $tbl = $_POST["tbl"];
+  $lst = $_POST["lst"];
   switch ($tbl) {
     case 'bw':
-      $sql = "SELECT `id`,`fname`,`lname`,`b_points` FROM `users` WHERE `b_points`>0 ORDER BY `b_points` DESC";
+      $sql = "SELECT * FROM `users` WHERE `b_points`>0 ORDER BY `b_points` DESC";
       break;
     case 'bt':
-      $sql = "SELECT `id`,`fname`,`lname`,`b_total` FROM `users` WHERE `b_total`>0 ORDER BY `b_total` DESC";
+      $sql = "SELECT * FROM `users` WHERE `b_total`>0 ORDER BY `b_total` DESC";
       break;
     case 'ew':
-      $sql = "SELECT `id`,`fname`,`lname`,`e_points` FROM `users` WHERE `e_points`>0 ORDER BY `e_points` DESC";
+      $sql = "SELECT * FROM `users` WHERE `e_points`>0 ORDER BY `e_points` DESC";
       break;
     case 'et':
-      $sql = "SELECT `id`,`fname`,`lname`,`e_total` FROM `users` WHERE `e_total`>0 ORDER BY `e_total` DESC";
+      $sql = "SELECT * FROM `users` WHERE `e_total`>0 ORDER BY `e_total` DESC";
       break;
   }
   try {
@@ -23,24 +24,43 @@
     $stmt->setFetchMode(PDO::FETCH_ASSOC);
     $i = 1;
     while ($row = $stmt->fetch()){
-      if ($i < 4) {
-        switch ($tbl) {
-          case 'bw':
-            $points = $row['b_points'];
-            break;
-          case 'bt':
-            $points = $row['b_total'];
-            break;
-          case 'ew':
-            $points = $row['e_points'];
-            break;
-          case 'et':
-            $points = $row['e_total'];
-            break;
+      if ($lst == "nor") {
+        if ($i < 4) {
+          switch ($tbl) {
+            case 'bw':
+              $points = $row['b_points'];
+              break;
+            case 'bt':
+              $points = $row['b_total'];
+              break;
+            case 'ew':
+              $points = $row['e_points'];
+              break;
+            case 'et':
+              $points = $row['e_total'];
+              break;
+          }
+          array_push($data,array($i,$row['fname']." ".$row['lname'],$points));
         }
-        array_push($data,array($i,$row['fname']." ".$row['lname'],$points));
+        if ($log_id == $row['id'] && $i > 3) {
+          switch ($tbl) {
+            case 'bw':
+              $points = $row['b_points'];
+              break;
+            case 'bt':
+              $points = $row['b_total'];
+              break;
+            case 'ew':
+              $points = $row['e_points'];
+              break;
+            case 'et':
+              $points = $row['e_total'];
+              break;
+          }
+          array_push($data,array($i,$row['fname']." ".$row['lname'],$points));
+        }
       }
-      if ($log_id == $row['id'] && $i > 3) {
+      if ($lst == "mdl") {
         switch ($tbl) {
           case 'bw':
             $points = $row['b_points'];
@@ -55,7 +75,7 @@
             $points = $row['e_total'];
             break;
         }
-        array_push($data,array($i,$row['fname']." ".$row['lname'],$points));
+        array_push($data,array($i,$row['fname']." ".$row['lname'],$row['city'],$row['teacher'],$points));
       }
       $i++;
     }
