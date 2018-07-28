000f555369f978468a5ee82f1adc1218689b7c40
boca-abdo <boca.abdo@gmail.com>
2018-07-28 19:10:52 +0100

    Update mul 1 fr page

--- ar/mul1.php
+++ ar/mul1.php
@@ -3,22 +3,22 @@
 				<hr>
 				<p class="lead">نعتمد في هذه الطريقة على الجمع المتكرر<span class="font-weight-bold">لا حاجة لحفظ جدول الضرب بهذه الطريقة.</span></p>
 				<p class="lead font-weight-bold text-danger"><span class="font-weight-bold">من المهم جدا اتقان المبادلات قبل البدء في تقنية الضرب بالجمع المتكرر </p>
-				<ul>
-          <li><h2 class="text-right">الضرب في 2</h2>
+				<ul class="list-unstyled">
+          <li><h2 class="h2">الضرب في 2</h2>
             <p class="lead">لضرب عدد في 2 نقوم بتمثيله ثم نضيف إليه نفسه</p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_2.gif" alt="mul1_2" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 4</h2>
+					<li><h2 class="h2">الضرب في 4</h2>
             <p class="lead">لضرب عدد في 4 نقوم بتمثيله ثم نضيف اليه نفسه، ثم نضيف للناتج نفسه مرة أخرى</p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_4.gif" alt="mul1_4" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 8</h2>
+					<li><h2 class="h2">الضرب في 8</h2>
             <p class="lead">لضرب عدد في 4 نقوم بتمثيله ثم نضيف اليه نفسه، ثم نضيف للناتج نفسه مرة أخرى، ثم نضيف للناتج نفسه مرة ثالثة</p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
@@ -27,42 +27,42 @@
 					<p class="lead font-weight-bold mt-3">خلال الحالات الثلاث السابقة يكون التعامل كليا مع المعداد، حيث لا نحتاج تذكر العدد أو كتابته. نقوم فقط بتمثيل العدد ثم نشتغل على كل عمود بإضافة العدد الممثل عليه لنفسه مرة (في 2) أو مرتين (في 4) أو ثلاث (في 8). </p>
 					<p class="lead font-weight-bold text-danger">يستحسن دائما البدء من اليسار خلال عملية الجمع المتكرر خصوصا في حالة استعمال المبادلة بعشرة </p>
 					<hr>
-					<li><h2 class="text-right">الضرب في 5</h2>
+					<li><h2 class="h2">الضرب في 5</h2>
             <p class="lead">لضرب عدد في 5 نتبع طريقة الضرب في 4 ثم نضيف للناتج العدد اﻷصلي </p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_5.gif" alt="mul1_5" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 3</h2>
+					<li><h2 class="h2">الضرب في 3</h2>
             <p class="lead">لضرب عدد في 3 نضيف اليه نفسه ثم نضيف للناتج العدد اﻷصلي </p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_3.gif" alt="mul1_3" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 6</h2>
+					<li><h2 class="h2">الضرب في 6</h2>
             <p class="lead">لضرب عدد في 6 نضيف اليه نفسه ثم نضيف للناتج العدد اﻷصلي ، ثم نضيف للناتج نفسه  </p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_6.gif" alt="mul1_6" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 7</h2>
+					<li><h2 class="h2">الضرب في 7</h2>
             <p class="lead">لضرب عدد في 7 نتبع طريقة الضرب في 6 ثم نضيف للناتج العدد اﻷصلي  </p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_7.gif" alt="mul1_7" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في مضاعفات 10،100،...</h2>
+					<li><h2 class="h2">الضرب في 10،100،...</h2>
             <p class="lead">لضرب عدد في مضاعفات  10، 100، 1000 … نترك اﻷعمدة الأولى فارغة بحسب عدد اﻷصفار، نترك عمود واحد في حالة الضرب في 10، وعمودين في حالة الضرب في 100 وهكذا. ثم نقوم بضرب العدد في الرقم اﻷخير بعد اﻷصفار بطريقة الجمع المتكرر التي تعلمناها في الأمثلة أعلاه. </p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_10.gif" alt="mul1_10" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 9</h2>
+					<li><h2 class="h2">الضرب في 9</h2>
             <p class="lead">قد تلاحظون أننا بدأنا بالضرب في 10 قبل الضرب في 9، هذا صحيح لأن قاعدة الضرب في 9 تعتمد على الطرح وليس على الجمع، أي أنه اذا اردنا ضرب عدد في 9 نقوم بضربه في 10 ثم نطرح من الناتج العدد اﻷصلي. </p>
 						<p class="text-danger font-weight-bold">لاحظ المثال</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
--- fr/mul1.php
+++ fr/mul1.php
@@ -1,101 +1,99 @@
   <?php include 'assets/header.php' ?>
-        <h1 class="h1">الضرب بالطريقة اﻷولى</h1>
+        <h1 class="h1 text-uppercase">Multiplication 1</h1>
 				<hr>
-				<p class="lead">نعتمد في هذه الطريقة على الجمع المتكرر<span class="font-weight-bold">لا حاجة لحفظ جدول الضرب بهذه الطريقة.</span></p>
-				<p class="lead font-weight-bold text-danger"><span class="font-weight-bold">من المهم جدا اتقان المبادلات قبل البدء في تقنية الضرب بالجمع المتكرر </p>
-				<ul>
-          <li><h2 class="text-right">الضرب في 2</h2>
-            <p class="lead">لضرب عدد في 2 نقوم بتمثيله ثم نضيف إليه نفسه</p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+				<p class="lead">avec cette méthode, on a plus besoin d'apprendre les tableaux de multiplication par coeur, on utilisera l'addition et la soustraction de façon répétitive.</p>
+				<p class="lead font-weight-bold text-danger">Il est fortement déconseillé de travailler avec cette methode sans avoir bien maîtrisé les règles vues précedemment</p>
+				<ul class="list-unstyled">
+          <li><h2 class="h2">Multiplication par 2</h2>
+            <p class="lead">Pour multiplier un nombre par deux on le représente puis on lui ajoute soi même.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_2.gif" alt="mul1_2" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 4</h2>
-            <p class="lead">لضرب عدد في 4 نقوم بتمثيله ثم نضيف اليه نفسه، ثم نضيف للناتج نفسه مرة أخرى</p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 4</h2>
+            <p class="lead">Pour multiplier un nombre par quatre on le représente puis on lui ajoute soi même, puis on ajoute au résultat soi même aussi.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_4.gif" alt="mul1_4" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 8</h2>
-            <p class="lead">لضرب عدد في 4 نقوم بتمثيله ثم نضيف اليه نفسه، ثم نضيف للناتج نفسه مرة أخرى، ثم نضيف للناتج نفسه مرة ثالثة</p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 8</h2>
+            <p class="lead">Pour multiplier un nombre par huit on utilise la même méthode précedente mais à trois niveaux.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_8.gif" alt="mul1_8" class="img-fluid center-block border border-dark mt-3">
           </li>
-					<p class="lead font-weight-bold mt-3">خلال الحالات الثلاث السابقة يكون التعامل كليا مع المعداد، حيث لا نحتاج تذكر العدد أو كتابته. نقوم فقط بتمثيل العدد ثم نشتغل على كل عمود بإضافة العدد الممثل عليه لنفسه مرة (في 2) أو مرتين (في 4) أو ثلاث (في 8). </p>
-					<p class="lead font-weight-bold text-danger">يستحسن دائما البدء من اليسار خلال عملية الجمع المتكرر خصوصا في حالة استعمال المبادلة بعشرة </p>
 					<hr>
-					<li><h2 class="text-right">الضرب في 5</h2>
-            <p class="lead">لضرب عدد في 5 نتبع طريقة الضرب في 4 ثم نضيف للناتج العدد اﻷصلي </p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 5</h2>
+            <p class="lead">Pour multiplier un nombre par cinq on le multiplie par quatre et on lui ajoute le nombre initial.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_5.gif" alt="mul1_5" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 3</h2>
-            <p class="lead">لضرب عدد في 3 نضيف اليه نفسه ثم نضيف للناتج العدد اﻷصلي </p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 3</h2>
+            <p class="lead">Pour multiplier un nombre par trois on lui ajoute soi même deux fois.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_3.gif" alt="mul1_3" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 6</h2>
-            <p class="lead">لضرب عدد في 6 نضيف اليه نفسه ثم نضيف للناتج العدد اﻷصلي ، ثم نضيف للناتج نفسه  </p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 6</h2>
+            <p class="lead">Pour multiplier un nombre par six on le multiplie par trois puis on ajoute au résultat soi même.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_6.gif" alt="mul1_6" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 7</h2>
-            <p class="lead">لضرب عدد في 7 نتبع طريقة الضرب في 6 ثم نضيف للناتج العدد اﻷصلي  </p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 7</h2>
+            <p class="lead">Pour multiplier un nombre par sept on le multiplie par six et on lui ajoute le nombre initial.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_7.gif" alt="mul1_7" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في مضاعفات 10،100،...</h2>
-            <p class="lead">لضرب عدد في مضاعفات  10، 100، 1000 … نترك اﻷعمدة الأولى فارغة بحسب عدد اﻷصفار، نترك عمود واحد في حالة الضرب في 10، وعمودين في حالة الضرب في 100 وهكذا. ثم نقوم بضرب العدد في الرقم اﻷخير بعد اﻷصفار بطريقة الجمع المتكرر التي تعلمناها في الأمثلة أعلاه. </p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 10,100 ...</h2>
+            <p class="lead">Pour multiplier un nombre par 10,100 ..., on laisse vide les premiers colonnes au nombre des zéros (1 pour 10, 2 pour 100 ...), puis on représente le nombre sur les autres colonnes du boulier.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_10.gif" alt="mul1_10" class="img-fluid center-block border border-dark mt-3">
           </li>
 					<hr>
-					<li><h2 class="text-right">الضرب في 9</h2>
-            <p class="lead">قد تلاحظون أننا بدأنا بالضرب في 10 قبل الضرب في 9، هذا صحيح لأن قاعدة الضرب في 9 تعتمد على الطرح وليس على الجمع، أي أنه اذا اردنا ضرب عدد في 9 نقوم بضربه في 10 ثم نطرح من الناتج العدد اﻷصلي. </p>
-						<p class="text-danger font-weight-bold">لاحظ المثال</p>
+					<li><h2 class="h2">Multiplication par 9</h2>
+            <p class="lead">Pour multiplier un nombre par neuf on le multiplie par dix puis on en soustrait le nombre initial.</p>
+						<p class="text-danger font-weight-bold">Exemple</p>
 						<i class="fas fa-2x fa-hand-point-down d-block animated fadeInDown infinite"></i>
 						<img src="../../images/mul1_9.gif" alt="mul1_9" class="img-fluid center-block border border-dark mt-3">
           </li>
 				</ul>
 				<hr>
-				<h2 class="h2 text-center animated flash infinite">احصل على شهادة التدريب</h2>
-				<h5 class="h5 my-3">قم بانهاء جميع التمارين بعد نهاية كل وحدة للحصول على شهادة التدريب</h5>
+        <h2 class="h2 text-center text-uppercase animated flash infinite">Obtener votre certificat</h2>
+				<h5 class="h5 my-3">Complétez les exercices de chaque unité pour obtenir votre certificat.</h5>
 				<p class="lead animated flash infinite text-danger font-weight-bold"></p>
 				<div class="row border border-dark mx-3">
-					<ul class="nav nav-fill nav-tabs border-bottom-0 w-100 p-0" id="exercices" role="tablist">
+          <ul class="nav nav-fill nav-tabs border-bottom-0 text-uppercase w-100 p-0" id="exercices" role="tablist">
 					  <li class="nav-item">
-					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="0" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">التمرين </span>1</a>
+					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="0" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">exercice </span>1</a>
 					  </li>
 					  <li class="nav-item">
-					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="1" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">التمرين </span>2</a>
+					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="1" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">exercice </span>2</a>
 					  </li>
 					  <li class="nav-item">
-					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="2" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">التمرين </span>3</a>
+					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="2" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">exercice </span>3</a>
 					  </li>
 					  <li class="nav-item">
-					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="3" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">التمرين </span>4</a>
+					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="3" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">exercice </span>4</a>
 					  </li>
 					  <li class="nav-item">
-					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="4" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">التمرين </span>5</a>
+					    <a class="btn btn-block btn-outline-dark border-right-0 border-left-0 border-top-0 rounded-0" data-ex="4" data-toggle="tab" role="tab" aria-selected="false" style="box-shadow: none"><span class="d-none d-md-inline">exercice </span>5</a>
 					  </li>
 					</ul>
 					<div class="tab-content w-100 text-center" id="exercices_content">
 						<div class="tab-pane fade" role="tabpanel">
-							<h5 class="h3 my-3">التمرين 1</h5>
-							<p class="lead">قم بالحسابات اللازمة على المعداد ثم اضغط على زر التحقق</p>
-							<div class="py-3">
+              <h5 class="h3 my-3 text-uppercase">Exercice 1</h5>
+							<p class="lead">Fait les calculs sur le boulier puis vérifie</p>
+							<div class="py-3" dir="rtl">
 								<div class="row no-gutters justify-content-center h1 font-weight-bold">
 									<div class="nbr1 col-auto">
 										<span></span>
@@ -107,15 +105,15 @@
 								</div>
 								<div id="abacus" class="my-3" style="height: 143px"></div>
 								<div id="status"></div>
-								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تحقق<i class="fas fa-check mr-2"></i></button>
-								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تغيير العدد<i class="fas fa-undo mr-2"></i></button>
+								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-check ml-2"></i>vérifier</button>
+								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-undo ml-2"></i>changer</button>
 							</div>
 							<p></p>
 						</div>
 					  <div class="tab-pane fade" role="tabpanel">
-							<h5 class="h3 my-3">التمرين 2</h5>
-							<p class="lead">قم بالحسابات اللازمة على المعداد ثم اضغط على زر التحقق</p>
-							<div class="py-3">
+              <h5 class="h3 my-3 text-uppercase">Exercice 2</h5>
+							<p class="lead">Fait les calculs sur le boulier puis vérifie</p>
+							<div class="py-3" dir="rtl">
 								<div class="row no-gutters justify-content-center h1 font-weight-bold">
 									<div class="nbr1 col-auto">
 										<span></span><span></span>
@@ -127,15 +125,15 @@
 								</div>
 								<div id="abacus" class="my-3" style="height: 143px"></div>
 								<div id="status"></div>
-								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تحقق<i class="fas fa-check mr-2"></i></button>
-								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تغيير العدد<i class="fas fa-undo mr-2"></i></button>
+								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-check ml-2"></i>vérifier</button>
+								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-undo ml-2"></i>changer</button>
 							</div>
 							<p></p>
 					  </div>
 					  <div class="tab-pane fade" role="tabpanel">
-							<h5 class="h3 my-3">التمرين 3</h5>
-							<p class="lead">قم بالحسابات اللازمة على المعداد ثم اضغط على زر التحقق</p>
-							<div class="py-3">
+              <h5 class="h3 my-3 text-uppercase">Exercice 3</h5>
+							<p class="lead">Fait les calculs sur le boulier puis vérifie</p>
+							<div class="py-3" dir="rtl">
 								<div class="row no-gutters justify-content-center h1 font-weight-bold">
 									<div class="nbr1 col-auto">
 										<span></span><span></span><span></span>
@@ -147,15 +145,15 @@
 								</div>
 								<div id="abacus" class="my-3" style="height: 143px"></div>
 								<div id="status"></div>
-								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تحقق<i class="fas fa-check mr-2"></i></button>
-								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تغيير العدد<i class="fas fa-undo mr-2"></i></button>
+								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-check ml-2"></i>vérifier</button>
+								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-undo ml-2"></i>changer</button>
 							</div>
 							<p></p>
 					  </div>
 					  <div class="tab-pane fade" role="tabpanel">
-							<h5 class="h3 my-3">التمرين 4</h5>
-							<p class="lead">قم بالحسابات اللازمة على المعداد ثم اضغط على زر التحقق</p>
-							<div class="py-3">
+              <h5 class="h3 my-3 text-uppercase">Exercice 4</h5>
+							<p class="lead">Fait les calculs sur le boulier puis vérifie</p>
+							<div class="py-3" dir="rtl">
 								<div class="row no-gutters justify-content-center h1 font-weight-bold">
 									<div class="nbr1 col-auto">
 										<span></span><span></span><span></span><span></span>
@@ -167,15 +165,15 @@
 								</div>
 								<div id="abacus" class="my-3" style="height: 143px"></div>
 								<div id="status"></div>
-								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تحقق<i class="fas fa-check mr-2"></i></button>
-								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تغيير العدد<i class="fas fa-undo mr-2"></i></button>
+								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-check ml-2"></i>vérifier</button>
+								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-undo ml-2"></i>changer</button>
 							</div>
 							<p></p>
 					  </div>
 					  <div class="tab-pane fade" role="tabpanel">
-							<h5 class="h3 my-3">التمرين 5</h5>
-							<p class="lead">قم بالحسابات اللازمة على المعداد ثم اضغط على زر التحقق</p>
-							<div class="py-3">
+              <h5 class="h3 my-3 text-uppercase">Exercice 5</h5>
+							<p class="lead">Fait les calculs sur le boulier puis vérifie</p>
+							<div class="py-3" dir="rtl">
 								<div class="row no-gutters justify-content-center h1 font-weight-bold">
 									<div class="nbr1 col-auto">
 										<span></span><span></span><span></span><span></span><span></span>
@@ -187,8 +185,8 @@
 								</div>
 								<div id="abacus" class="my-3" style="height: 143px"></div>
 								<div id="status"></div>
-								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تحقق<i class="fas fa-check mr-2"></i></button>
-								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark rounded-0 my-2" style="box-shadow: none">تغيير العدد<i class="fas fa-undo mr-2"></i></button>
+								<button type="button" class="col-8 col-sm-4 col-md-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-check ml-2"></i>vérifier</button>
+								<button type="button" class="col-8 col-sm-4 col-lg-2 btn btn-outline-dark text-uppercase rounded-0 my-2" style="box-shadow: none"><i class="fas fa-undo ml-2"></i>changer</button>
 							</div>
 							<p></p>
 					  </div>
