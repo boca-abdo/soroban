55883bdc9eac9eb629c1d2ce2dd6af8f33fe4cd2
boca-abdo <boca.abdo@gmail.com>
2018-09-14 17:58:45 +0100

    remove tab menu

--- ar/newchallenge.php
+++ ar/newchallenge.php
@@ -4,69 +4,42 @@
 			    <source src="../beep.ogg" type="audio/mpeg">
 			    <source src="../beep.wav" type="audio/mpeg">
 			  </audio>
-				<ul class="nav justify-content-center nav-tabs border-bottom-0 p-0 mt-3" id="main" role="tablist">
-				  <li class="nav-item col-6 col-sm-3 pb-2">
-				    <a class="btn btn-block btn-outline-dark rounded-0" id="add-tab" data-toggle="tab" href="#add" role="tab" aria-controls="add" aria-selected="true" style="box-shadow: none">الجمع</a>
-				  </li>
-				  <li class="nav-item col-6 col-sm-3 pb-2">
-				    <a class="btn btn-block btn-outline-dark rounded-0" id="sub-tab" data-toggle="tab" href="#sub" role="tab" aria-controls="sub" aria-selected="false" style="box-shadow: none">الطرح</a>
-				  </li>
-				  <li class="nav-item col-6 col-sm-3 pb-2">
-				    <a class="btn btn-block btn-outline-dark rounded-0" id="mul-tab" data-toggle="tab" href="#mul" role="tab" aria-controls="mul" aria-selected="false" style="box-shadow: none">الضرب</a>
-				  </li>
-				  <li class="nav-item col-6 col-sm-3 pb-2">
-				    <a class="btn btn-block btn-outline-dark rounded-0" id="div-tab" data-toggle="tab" href="#div" role="tab" aria-controls="div" aria-selected="false" style="box-shadow: none">القسمة</a>
-				  </li>
-				</ul>
-				<div class="tab-content" id="main_content">
-				  <div class="tab-pane fade pt-3" id="add" role="tabpanel">
-            <div class="row justify-content-center">
-            <?php
-              $json_data = file_get_contents('../json/challenge.json');
-              $data = json_decode($json_data, true);
-              foreach ($data["add"] as $key => $value) {
-            ?>
-              <div class="col-sm-6 col-md-4 col-lg-3">
-                <div class="card bg-dark text-warning rounded-0 d-none mb-3" style="box-shadow: -5px 5px 15px #20343B">
-                  <div class="card-header text-right">
-                    <?php echo ($log_row['chal_level'] <= $value['level']) ? "المستوى ".$key : "المستوى ".$key."<i class='far fa-lg fa-check-circle float-left'></i>" ?>
-                  </div>
-                  <div class="card-body py-0">
-										<ul class="list-group p-0">
-										  <li class="list-group-item d-flex justify-content-between align-items-center rounded-0 bg-transparent border-warning">
-										    عدد اﻷرقام
-										    <span class="badge badge-warning badge-pill"><?php echo strlen((string)$value['rank']) ?></span>
-										  </li>
-										  <li class="list-group-item d-flex justify-content-between align-items-center bg-transparent border-warning">
-										    عدد اﻷعداد
-										    <span class="badge badge-warning badge-pill"><?php echo $value['numbers'] ?></span>
-										  </li>
-										  <li class="list-group-item d-flex justify-content-between align-items-center rounded-0 bg-transparent border-warning">
-										    عدد الثواني
-										    <span class="badge badge-warning badge-pill"><?php echo $value['speed']/1000 ?></span>
-										  </li>
-										</ul>
-                  </div>
-                  <div class="card-footer">
-                    <button type="button" data-level="<?php echo $value['level'] ?>" data-points="<?php echo $value['points'] ?>" class="px-5 rounded-0 btn btn-outline-warning <?php echo ($log_row['chal_level'] < $value['level']) ? "disabled" : "" ?>" style="box-shadow: none"><?php echo ($log_row['chal_level'] < $value['level']) ? "غير متاح" : "أبدأ" ?></button>
-                  </div>
-                </div>
+        <div class="row justify-content-center pt-5" id="levels">
+        <?php
+          $json_data = file_get_contents('../json/challenge.json');
+          $data = json_decode($json_data, true);
+          foreach ($data as $key => $value) {
+        ?>
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="card bg-dark text-warning rounded-0 d-none mb-3" style="box-shadow: -5px 5px 15px #20343B">
+              <div class="card-header text-right">
+                <?php echo ($log_row['chal_level'] <= $value['level']) ? "المستوى ".$key : "المستوى ".$key."<i class='far fa-lg fa-check-circle float-left'></i>" ?>
               </div>
-            <?php
-              }
-            ?>
+              <div class="card-body py-0">
+								<ul class="list-group p-0">
+								  <li class="list-group-item d-flex justify-content-between align-items-center rounded-0 bg-transparent border-warning">
+								    عدد اﻷرقام
+								    <span class="badge badge-warning badge-pill"><?php echo strlen((string)$value['rank']) ?></span>
+								  </li>
+								  <li class="list-group-item d-flex justify-content-between align-items-center bg-transparent border-warning">
+								    عدد اﻷعداد
+								    <span class="badge badge-warning badge-pill"><?php echo $value['numbers'] ?></span>
+								  </li>
+								  <li class="list-group-item d-flex justify-content-between align-items-center rounded-0 bg-transparent border-warning">
+								    عدد الثواني
+								    <span class="badge badge-warning badge-pill"><?php echo $value['speed']/1000 ?></span>
+								  </li>
+								</ul>
+              </div>
+              <div class="card-footer">
+                <button type="button" data-level="<?php echo $value['level'] ?>" data-points="<?php echo $value['points'] ?>" class="px-5 rounded-0 btn btn-outline-warning <?php echo ($log_row['chal_level'] < $value['level']) ? "disabled" : "" ?>" style="box-shadow: none"><?php echo ($log_row['chal_level'] < $value['level']) ? "غير متاح" : "أبدأ" ?></button>
+              </div>
             </div>
-					</div>
-				  <div class="tab-pane fade pt-3" id="sub" role="tabpanel">
-
-					</div>
-				  <div class="tab-pane fade pt-3" id="mul" role="tabpanel">
-
-					</div>
-				  <div class="tab-pane fade pt-3" id="div" role="tabpanel">
-
-					</div>
-				</div>
+          </div>
+        <?php
+          }
+        ?>
+        </div>
 			</div>
 		</div>
 		<?php include 'assets/footer.php' ?>
@@ -75,7 +48,7 @@
     $(document).ready(function() {
 			$("#spinner").addClass("d-none");
 			$("a[href='challenge.php']").addClass("active");
-			var i,j,dig,num,spd,lvl,points,correct,tab,anim,answer,keyCode,rand,res,intvl,gen,clr,face;
+			var i,j,dig,num,spd,lvl,points,correct,anim,answer,keyCode,rand,res,intvl,gen,clr,face;
 			function nextLevel() {
 				$.ajax({
 					url: "../include/nextlevel.php",
@@ -156,18 +129,15 @@
           }
         },spd * 1000);
       }
-			$('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-        tab = $(this).attr('href');
-        j = 0;
-        anim = setInterval(function(){
-          $(tab).find("div.card:eq("+j+")").removeClass("d-none").addClass("animated fadeInRight");
-          j++;
-          if (j > $(tab).find("button").length) {
-            clearInterval(anim);
-          }
-        },250);
-			});
-			$(".tab-pane").on("click", "button", function() {
+      j = 0;
+      anim = setInterval(function(){
+        $("#levels").find("div.card:eq("+j+")").removeClass("d-none").addClass("animated fadeInRight");
+        j++;
+        if (j > $("#levels").find("button").length) {
+          clearInterval(anim);
+        }
+      },250);
+			$("#levels").on("click", "button", function() {
 				dig = Number($(this).closest(".card").find(".badge:first").text());
 				num = Number($(this).closest(".card").find(".badge:eq(1)").text());
 				spd = Number($(this).closest(".card").find(".badge:last").text());
