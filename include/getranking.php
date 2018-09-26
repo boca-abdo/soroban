a4a360f94850dd3e17650408f87e914352bf6177
boca-abdo <boca.abdo@gmail.com>
2018-09-26 19:17:54 +0100

    Update new version of challenge and dashboard

--- include/getranking.php
+++ include/getranking.php
@@ -2,12 +2,12 @@
   include 'user_check.php';
   $from = intval(protect($_POST['from']));
   $data = array();
-  $query = $srbn_con->query("SELECT fname,lname,points FROM users WHERE points > 0 ORDER BY 3 DESC LIMIT $from, 9");
+  $query = $srbn_con->query("SELECT `fname`,`lname`,`points`,`city`,`teacher` FROM `users` WHERE `points` > 0 ORDER BY 3 DESC LIMIT $from, 9");
   $query->setFetchMode(PDO::FETCH_ASSOC);
   while ($row = $query->fetch()) {
     $from++;
     $data[$from] = array();
-    array_push($data[$from],$row['fname'],$row['lname'],$row['points']);
+    array_push($data[$from],$row['fname'],$row['lname'],$row['city'],$row['teacher'],$row['points']);
     array_push($data,$data[$from]);
   }
   echo json_encode($data, true);
